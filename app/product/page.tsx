6285498bc2732b27e655e0ed9984137956dccab2
shakhobiddinkhusanov-debug <shakhobiddinkhusanov@gmail.com>
2026-04-27 16:41:09 +0500

    Delete app/product/page.tsx

--- app/product/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProductPage() {
-  return (
-    <main style={{ padding: "40px" }}>
-      <h1>Product page работает</h1>
-      <p>Карточка товара открылась.</p>
-    </main>
-  );
-}
